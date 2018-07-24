fff9a3664125ad1d27189376f3101932d8ff9a73
Safdar Kureishy <safdar.ureishy@gmail.com>
2018-07-24 08:16:51 +0000

    Fixed bug with get_prediction() method

--- train_utils.py
+++ train_utils.py
@@ -78,3 +78,47 @@ def train_model(input_to_softmax,
     # save model loss
     with open('results/'+pickle_path, 'wb') as f:
         pickle.dump(hist.history, f)
+        
+import numpy as np
+from utils import int_sequence_to_text
+from IPython.display import Audio
+
+def get_predictions(index, partition, input_to_softmax, model_path, spectogram=True, mfcc_dim=13):
+    """ Print a model's decoded predictions
+    Params:
+        index (int): The example you would like to visualize
+        partition (str): One of 'train' or 'validation'
+        input_to_softmax (Model): The acoustic model
+        model_path (str): Path to saved acoustic model's weights
+    """
+    # load the train and test data
+    data_gen = AudioGenerator(spectrogram=spectogram, mfcc_dim=mfcc_dim)
+    data_gen.load_train_data()
+    data_gen.load_validation_data()
+    
+    # obtain the true transcription and the audio features 
+    if partition == 'validation':
+        transcr = data_gen.valid_texts[index]
+        audio_path = data_gen.valid_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    elif partition == 'train':
+        transcr = data_gen.train_texts[index]
+        audio_path = data_gen.train_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    else:
+        raise Exception('Invalid partition!  Must be "train" or "validation"')
+        
+    # obtain and decode the acoustic model's predictions
+    input_to_softmax.load_weights('results/'+model_path)
+    prediction = input_to_softmax.predict(np.expand_dims(data_point, axis=0))
+    output_length = [input_to_softmax.output_length(data_point.shape[0])] 
+    pred_ints = (K.eval(K.ctc_decode(
+                prediction, output_length)[0][0])+1).flatten().tolist()
+    
+    # play the audio file, and display the true and predicted transcriptions
+    print('-'*80)
+    Audio(audio_path)
+    print('True transcription:\n' + '\n' + transcr)
+    print('-'*80)
+    print('Predicted transcription:\n' + '\n' + ''.join(int_sequence_to_text(pred_ints)))
+    print('-'*80)
