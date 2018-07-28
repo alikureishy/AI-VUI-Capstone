7a1de098dc86d46056fc8fd59a8436d77f9521a1
Safdar Kureishy <safdar.ureishy@gmail.com>
2018-07-28 04:03:00 +0000

    Added a few more models

--- trainer_b.sh
+++ trainer_b.sh
@@ -3,16 +3,10 @@
 #################################################################
 
 # Increase # of epochs:
-python trainer.py -o results -i 18 -cf 80 -ck 10 -cs 2 -cp same -cd 0.25 -rl 3 -ru 200 200 200 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 1 1 1 -dd 0.25 -e 35 &>> out.txt
-BACK_PID=$!
-while kill -0 $BACK_PID ; do
-    echo "Waiting for #1 to complete..."
-    sleep 10
-    # You can add a timeout here if you want
-done
-
-# Decrease dropout
-python trainer.py -o results -i 19 -cf 80 -ck 10 -cs 2 -cp same -cd 0.15 -rl 3 -ru 200 200 200 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 1 1 1 -dd 0.15 -e 35 &>> out.txt
+# This achieves a training loss of 65 units and validation loss of about 87 units in 35 epochs.
+# The training loss could be further optimized with more training and  a higher dropout would also allow the two to converge closer.
+# So, decreasing dropout will not be appropriate for this exercise.
+python trainer.py -o results -i 19 -cf 200 -ck 20 -cs 1 -cp same -cd 0.25 -rl 2 -ru 250 250 -rb 1 1 -rd 0.25 0.25 -rc 2 2 -dd 0.25 -e 30 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
@@ -21,7 +15,7 @@ while kill -0 $BACK_PID ; do
 done
 
 # Change to LSTM
-python trainer.py -o results -i 20 -cf 80 -ck 10 -cs 2 -cp same -cd 0.15 -rl 3 -ru 200 200 200 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 2 2 2 -dd 0.15 -e 35 &>> out.txt
+python trainer.py -o results -i 20 -cf 80 -ck 10 -cs 2 -cp same -cd 0.25 -rl 3 -ru 200 200 200 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 2 2 2 -dd 0.25 -e 40 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
@@ -30,7 +24,7 @@ while kill -0 $BACK_PID ; do
 done
 
 # Increase # of recurrent units
-python trainer.py -o results -i 21 -cf 80 -ck 10 -cs 2 -cp same -cd 0.15 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 2 2 2 -dd 0.15 -e 35 &>> out.txt
+python trainer.py -o results -i 21 -cf 80 -ck 10 -cs 2 -cp same -cd 0.25 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 2 2 2 -dd 0.25 -e 40 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
@@ -39,7 +33,7 @@ while kill -0 $BACK_PID ; do
 done
 
 # Increase # of conv filters
-python trainer.py -o results -i 22 -cf 200 -ck 10 -cs 2 -cp same -cd 0.15 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 2 2 2 -dd 0.15 -e 35 &>> out.txt
+python trainer.py -o results -i 22 -cf 200 -ck 10 -cs 2 -cp same -cd 0.25 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 2 2 2 -dd 0.25 -e 40 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
@@ -48,7 +42,7 @@ while kill -0 $BACK_PID ; do
 done
 
 # Increase size of conv kernel
-python trainer.py -o results -i 23 -cf 200 -ck 20 -cs 2 -cp same -cd 0.15 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 2 2 2 -dd 0.15 -e 35 &>> out.txt
+python trainer.py -o results -i 23 -cf 200 -ck 20 -cs 2 -cp same -cd 0.25 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 2 2 2 -dd 0.25 -e 35 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
@@ -57,7 +51,7 @@ while kill -0 $BACK_PID ; do
 done
 
 # Decrease stride
-python trainer.py -o results -i 24 -cf 200 -ck 20 -cs 1 -cp same -cd 0.15 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.15 0.15 0.15 -rc 2 2 2 -dd 0.15 -e 35 &>> out.txt
+python trainer.py -o results -i 24 -cf 200 -ck 20 -cs 1 -cp same -cd 0.25 -rl 3 -ru 250 250 250 -rb 1 1 1 -rd 0.25 0.25 0.25 -rc 2 2 2 -dd 0.25 -e 35 &>> out.txt
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
     echo "Waiting for #1 to complete..."
